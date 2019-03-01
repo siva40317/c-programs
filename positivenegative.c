97ba423f93295521f37ca1b740e3e23254515ed8
siva40317 <48128680+siva40317@users.noreply.github.com>
2019-03-01 20:04:09 +0530

    Add files via upload

---
+++ positivenegative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	
+    int n ;
+    
+   scanf("%d",&n);
+   if(n==0)
+   {
+   	printf("Zero");
+   }
+   else if(n<0)
+   {
+   	printf("Negative");
+   }
+   else
+   {
+   	printf("Positive");
+   }
+	return 0;
+}
